02b2dfa18e58e6ff9fcea67ad6dc96f778150377
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-16 11:48:57 +0530

    Update ScrollCategory.jsx

--- src/component/ScrollCategory.jsx
+++ src/component/ScrollCategory.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import BlogTitleContainer from './BlogTitleContainer'
 const ScrollCategory = () => {
+  const elementRef = useRef(null);
+  const fun1 =(e)=>{
+    elementRef.current.scrollLeft += 20;
+  }
   return (
     <>
       <div className='scroll_category'>
-        <ul class="nav nav-pills" id="pills-tab" role="tablist">
+        <div className='scrollabsbuttonbox'>
+        <ul class="nav nav-pills" id="pills-tab " role="tablist" ref={elementRef}>
           <li class="nav-item" role="presentation">
             <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
           </li>
@@ -45,6 +50,9 @@ const ScrollCategory = () => {
             <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
           </li>
         </ul>
+        <span className='scrollabsbutton' onClick={()=> fun1() }> <i class="fa fa-chevron-right"></i> </span>
+        </div>
+        
         <div class="tab-content" id="pills-tabContent">
           <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
             <BlogTitleContainer />
