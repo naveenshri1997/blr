a99dc5508cbf7f3f0ef113ad6306bc1bcc3c9e6f
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-16 12:35:23 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,12 +4,10 @@ const Header = () => {
   return (
     <>
     <nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid">
+  <div class="container-fluid justify-content-start">
     <a class="navbar-brand" href="#"><img width="30px" src={logo} /></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">      
+    
+    <div>      
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />       
       </form>
